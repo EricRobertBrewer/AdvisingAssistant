07e6b8999aacb9f38869cf5c50ed3464c38091aa
Garrett Mulroney <garrettmulroney@unknown1040f384fc76.gateway.2wire.net>
2012-04-09 15:09:14 -0700

    Created Files for login view and new student view

---
+++ AdvisingAsistantNew/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  AdvisingAsistant
+//
+//  Created by Garrett Mulroney on 4/9/12.
+//  Copyright (c) 2012 Sonoma State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ AdvisingAsistantNew/NewStudentViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewStudentViewController.h
+//  AdvisingAsistant
+//
+//  Created by Garrett Mulroney on 4/9/12.
+//  Copyright (c) 2012 Sonoma State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewStudentViewController : UIViewController
+
+@end
