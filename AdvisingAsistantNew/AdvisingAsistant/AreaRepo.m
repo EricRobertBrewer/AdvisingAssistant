3caf175f00a91dabf6e36578db2cbf0748925c0f
student <student@ssu-cs-dar25-167-169.sonoma.edu>
2012-04-26 16:47:16 -0700

    Added arearepo code

--- AdvisingAsistantNew/AdvisingAsistant/AreaRepo.m
+++ AdvisingAsistantNew/AdvisingAsistant/AreaRepo.m
@@ -12,25 +12,51 @@ static AreaRepo *instance = nil;
 
 @implementation AreaRepo
 
+/*
+    SERIALIZING DICTIONARIES
+*/
+
+-(Area *)areaFromDict:(NSDictionary *)dict {
+    Area *area = [[Area alloc] init];
+    area.name = [dict objectForKey:@"Area"];
+    area.title = [dict objectForKey:@"Title"];
+    return [area autorelease];
+}
+
+-(NSArray *)areasFromDicts:(NSArray *)dicts {
+    NSMutableArray *areas = [NSMutableArray array];
+    for (NSDictionary *dict in dicts) {
+        [areas addObject:[self areaFromDict:dict]];
+    }
+    return areas;
+}
+
+/*
+    GETTING AREAS
+*/
+
 -(NSArray *)areasForArea:(Area *)area {
-	self.error = @"Not yet implemented";
-	return [NSArray array];
+	ConnectOptions *options = [ConnectOptions optionsWithUrl:@"getAreasForArea.php"];
+    [options.postData setValue:area.name forKey:@"Area"];
+	return [self areasFromDicts:[self connect:options]];
 }
 
 -(NSArray *)areasForDepartment:(Department *)department date:(SemesterDate)date {
-	self.error = @"Not yet implemented";
-	return [NSArray array];	
+	ConnectOptions *options = [ConnectOptions optionsWithUrl:@"getAreasForDepartment.php"];
+    [options.postData setValue:department.code forKey:@"DepartmentID"];
+	return [self areasFromDicts:[self connect:options]];
 }
 
 -(NSArray *)areasForGEPattern:(GEPattern)pattern date:(SemesterDate)date {
-	self.error = @"Not yet implemented";
-	return [NSArray array];
+	ConnectOptions *options = [ConnectOptions optionsWithUrl:@"getAreasForGEPattern.php"];
+    [options.postData setValue:FormatGEPattern(pattern) forKey:@"Pattern"];
+	return [self areasFromDicts:[self connect:options]];
 }
 
 
 /*
- SINGLETON STUFF
- */
+    SINGLETON STUFF
+*/
 
 +(AreaRepo *)defaultRepo {
 	if (instance == nil) {
