90f0cb4c0547490b490cbc7d1375c7d3e03b08d1
Ryan Shaul <terandle@gmail.com>
2012-04-27 13:39:07 -0700

    Login page starts with Keyboard open

--- AdvisingAsistantNew/LoginViewController.m
+++ AdvisingAsistantNew/LoginViewController.m
@@ -49,6 +49,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     repo = [StudentRepo defaultRepo];
+	[stndIDTextField becomeFirstResponder];
 }
 
 - (void)viewDidUnload
