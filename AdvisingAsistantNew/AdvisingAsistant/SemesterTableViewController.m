cee67d4655c3e9c15381f7a1ae5d9628c3c289d0
student <student@ssu-cs-dar25-167-181.sonoma.edu>
2012-04-30 14:28:28 -0700

    Warning image

--- AdvisingAsistantNew/AdvisingAsistant/SemesterTableViewController.m
+++ AdvisingAsistantNew/AdvisingAsistant/SemesterTableViewController.m
@@ -29,28 +29,6 @@
     return self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 1;
@@ -76,6 +54,8 @@
     units.text = [NSString stringWithFormat:@"%i", course.units];
     
     cell.accessoryView = units;
+    
+    [units release];
     
     return cell;
 }
