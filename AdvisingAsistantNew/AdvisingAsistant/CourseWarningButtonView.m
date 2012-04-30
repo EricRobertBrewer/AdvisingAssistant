59d8e10897a6b58e7feba9c7a2769246b82b2e55
Garrett Mulroney <garrettmulroney@wrls-105-174.sonoma.edu>
2012-04-30 15:35:47 -0700

    Added warning button

---
+++ AdvisingAsistantNew/AdvisingAsistant/CourseWarningButtonView.m
@@ -0,0 +1,31 @@
+//
+//  CourseWarningButtonView.m
+//  AdvisingAsistant
+//
+//  Created by Garrett Mulroney on 4/30/12.
+//  Copyright (c) 2012 Sonoma State University. All rights reserved.
+//
+
+#import "CourseWarningButtonView.h"
+
+@implementation CourseWarningButtonView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
